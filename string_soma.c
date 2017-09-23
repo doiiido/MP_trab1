f871b12376c92559e9651f8b20c145ad8185a1d3
doiiido <lincoln_abreu@aluno.unb.br>
2017-09-23 17:23:04 -0300

    passa teste 1 e 2

--- string_soma.c
+++ string_soma.c
@@ -1,5 +1,17 @@
 /*! \file */
+#include <stdio.h>
 
 int soma_string(const char * string_entrada ){
-	return 0;
+	char ref = '0';
+	const char * ptr = string_entrada;
+	char buff = '0';
+	int sum=0;
+	do{
+		if (buff != ',')
+			sum += buff - '0';
+		sscanf(ptr,"%c", &buff);
+		ptr ++;
+	}while(buff != '\n');
+	
+	return sum;
 }
